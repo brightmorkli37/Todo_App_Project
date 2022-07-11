28750918ad5cadc7bd35c7028bb8a165199f6a74
brightmorkli37 <brightmorkli37@gmail.com>
2022-07-11 21:25:21 +0000

    add api app

--- todo/admin.py
+++ todo/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Todo
 
 class TodoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', 'priority', 'important', 'created']
+    list_filter = ('important', 'status', 'priority')  
     readonly_fields = ('created',)
+    
 
 admin.site.register(Todo, TodoAdmin)
