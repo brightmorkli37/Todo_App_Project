1b48ab4ea5173a8bfe3b50dbc9ebc8122ef93256
brightmorkli37 <brightmorkli37@gmail.com>
2022-07-11 21:55:08 +0000

    restructure core project

--- project_root/urls.py
+++ project_root/urls.py
@@ -1,4 +1,4 @@
-"""todowoo URL Configuration
+"""project_root URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
